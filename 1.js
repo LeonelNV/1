4b3e2e4beb8a2cff70efccc2ed0885345f284369
Humbucker <90539813+LeonelNV@users.noreply.github.com>
2024-08-28 22:53:19 -0600

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,6 @@
+//1 - Conteo de caracteres
+function contarCaracteres(cadena){
+    return cadena.length;
+}
+ let mensaje = "Hola, soy leo";
+ console.log("Numero de caracteres de la cadena", contarCaracteres(mensaje));
